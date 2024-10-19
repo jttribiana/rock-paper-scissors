95cfa6ed71ac2ddaf6bbc34b7bffb71220fe5c56
jttribiana <179311210+jttribiana@users.noreply.github.com>
2024-10-19 11:40:23 +0800

    Rename js file to index.js. Add code lines

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+
+
+//creating a rock paper scissor project simplifying the problem
+//make random generating number code
+
+let x = Math.random();
+console.log(x)
+
+//make the random number form number up to 1-3
+
+let y = Math.floor(Math.random() * 3) + 1;
+console.log(y)
+
