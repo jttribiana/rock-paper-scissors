853752def092a2ec625752279e38e7b4d3652386
jttribiana <179311210+jttribiana@users.noreply.github.com>
2024-10-21 17:20:17 +0800

    Add codes for the exercise, figuring out how to tell code to count score each round

--- index.js
+++ index.js
@@ -1,14 +1,86 @@
-console.log("Hello World")
 
 
-//creating a rock paper scissor project simplifying the problem
-//make random generating number code
+//make the computer choose its random number that would eqaul to a string
 
-let x = Math.random();
-console.log(x)
 
-//make the random number form number up to 1-3
 
-let y = Math.floor(Math.random() * 3) + 1;
-console.log(y)
 
+/*
+const z = Math.floor(Math.random() * 3) + 1;
+
+function getComputerChoice(z){
+    if (z == 1) {
+        return "rock";
+    }
+
+    else if (z == 2){
+        return "paper";
+    }
+
+    else if (z == 3){
+        return "scissors";
+    }
+}
+console.log(getComputerChoice(z))
+
+
+//pseudo for humanchoice
+//add function where itd take human choie
+//this choice should return a number that would respond a valid score vs computer
+
+
+//create a button that would enter the user choice
+//make the user choice input recognised, use js
+
+let human = prompt
+function getHumanChoice(){
+    return 
+}
+
+/*
+document.getElementById("mySubmit").onclick = function(){
+
+    username = document.getElementById("myText").value;
+    
+    document.getElementById("header").textContent = `Hello ${username}`
+    
+    }
+
+*/
+
+//global scope
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
+function getComputerChoice(){
+    let computer = Math.floor(Math.random() * 3) + 1;
+    if (computer == 1) {
+        return "rock";
+    }
+
+    else if (computer == 2){
+        return "paper";
+    }
+
+    else if (computer == 3){
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let human = window.prompt("rock, paper, or scissors").toLowerCase();
+    return human;
+}
+console.log(getHumanChoice())
+
+
+//playround
+
+function playRound(humanChoice, computerChoice){
+    
+}
