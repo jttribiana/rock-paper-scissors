198252739934628d843c78e8a16024612bbfa671
jttribiana <179311210+jttribiana@users.noreply.github.com>
2024-10-30 15:15:10 +0800

    Improved the code to make sure the logic is working, remove the extra clg that confuses the code

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-//making computer choose
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"]
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-console.log(getComputerChoice())
+//global scope
+
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(`👩‍🦰: ${humanScore}`);
+console.log(`👾: ${computerScore}`);
 
 //making human choose
 
@@ -11,7 +12,40 @@ function getHumanChoice() {
     let human = window.prompt("rock, paper, or scissors").toLowerCase();
     return human;
 }
-console.log(getHumanChoice())
 
 
+//making computer choose
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"]
+    return choices[Math.floor(Math.random() * choices.length)];
+}
 
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+//logic to play single round
+
+function playRound (humanChoice, computerChoice){
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie! 🤝");
+    } else if (humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("You win! Scissors crushes paper! 😎");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock"){
+        console.log("You win! Paper crushes rock! 😎");
+        humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You win! Rock crushes scissors! 😎");
+        humanScore++;
+    } else  {
+        console.log("You lose! 🤬");
+        computerScore++;
+    }
+}
+
+
+console.log(`👩‍🦰: ${humanChoice}`);
+console.log(`👾: ${computerChoice}`);
+playRound (humanChoice,computerChoice) 
